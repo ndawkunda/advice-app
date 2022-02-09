541fc81f83194ebf3ffc56632eae08115ce3d64b
Ainina NDAW <ndawkunda@outlook.com>
2022-02-09 23:39:32 +0000

    button fix

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import './App.css';
 
 class App extends React.Component {
 
-  state = { advice: '' };
+	state = { advice: '' };
 
-  componentDidMount() {
-	  this.fetchAdvice();
-  }
+	componentDidMount() {
+		this.fetchAdvice();
+	}
 
-  fetchAdvice = () => {
-	  axios.get('https://api.adviceslip.com/advice')
-		.then((response) => {
-		  const { advice } = response.data.slip
-		  this.setState({ advice })
-		})
-		.catch((error) => {
-		  console.log(error);
-		});
-  }
+	fetchAdvice = () => {
+		axios.get('https://api.adviceslip.com/advice')
+			.then((response) => {
+				const { advice } = response.data.slip;
+				this.setState({ advice });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  render() {
-	const { advice } = this.state;
+	render() {
+		const { advice } = this.state;
 
-	return (
-	  <div className="app">
-		<div className="card">
-		  <h1 className="heading">{advice}</h1>
-		  <button className="button" onClick={this.fetchAdvice}>
-        <span>GIVE ME ADVICE !</span>
-		  </button>
-		</div>
-	  </div>
-	)
-  }
+		return (
+			<div className="app">
+				<div className="card">
+					<h1 className="heading">{advice}</h1>
+					<button className="button" onClick={this.fetchAdvice}>
+						<span>GIVE ME ADVICE !</span>
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
